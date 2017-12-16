2cf83b9c71eab7d419516c9203a6bd32bf25b049
jisub.yoon <yjisub11@gmail.com>
2017-12-16 21:45:54 +0900

    생활코딩 소스 일부 수정 및 코드 리뷰

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 알고리즘 스터디
+
+### by jsyoon from opentutorial source 
+
+>source 
+
+https://opentutorials.org/module/1335/8857#entirecode
--- src/com/acorn/linkedlist/LinkeList.java
+++ src/com/acorn/linkedlist/LinkeList.java
@@ -4,50 +4,65 @@ public class LinkeList {
     private Node head;
     private int size = 0;
 
-
+    LinkeList()
+    {
+    	System.out.println("call constructor ");
+    }
 
     public void addFirst(Node input) {
-//test git branch commit 
-System.out.println("call add First ");
+      
+    	//test git branch commit 
+        //  System.out.println("call add First ");
+    	Node firstNode=new Node();
+    	firstNode.setValue(null);
+    	size++;
+    	
     }
 
     public void addLast(Node input){
 
+    	
     }
 
     public void insert(int index, Node input){
 
+    	
     }
 
     public Node removeFirst(){
-
+    		
+    	
+    	return new Node();
+    		
     }
 
-    public Node removeLast(){
-
+    public void removeLast(){
+    		
     }
 
-    public Node remove(int index){
+    public void remove(int index){
 
     }
 
     public int size(){
-
+    		return size;
     }
 
     public Node get(int index){
-
+    		return null;
     }
 
     public boolean isEmpty(){
-
+    	Boolean result=Boolean.FALSE;
+    	
+        return result;
     }
 
     public boolean hasNext(){
-
+    	return Boolean.FALSE;
     }
 
     public void printList(){
-
+    		
     }
 }
