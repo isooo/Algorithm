5ae90a68a378db5d0e4bcc4ece28eede24576283
isooo <midnightmarching@gmail.com>
2018-01-06 14:13:32 +0900

    modify

--- DataStructure/src/linkedList/LinkeList.java
+++ DataStructure/src/linkedList/LinkeList.java
@@ -22,18 +22,19 @@ public class LinkeList {
 		if (this.head == null) {
 			this.head = newNode;
 		} else {
+			
 			newNode.setNext(this.head);
 			this.head = newNode;
 		}
 		this.size++;
-		printList();
+		printList("addFirst");
     }// addFirst()
 
     public void addLast(String newValue){
     	Node newNode = new Node(newValue);
     	
     	if (this.head == null) {
-    		head = newNode;
+    		this.head = newNode;
     	} else {
     		newNode.setNext(null);
     		Node findLast = this.head;
@@ -44,22 +45,81 @@ public class LinkeList {
     		findLast.setNext(newNode);
     	}
     	this.size++;
-    	printList();
+    	printList("addLast");
     }// addLast
 
+
     public void insert(int index, String inputValue){
+    	Node newNode = new Node(inputValue);
+    	
+    	if (this.head == null) {
+    		head = newNode;
+    	} else if (index == 1) {
+    		newNode.setNext(this.head);
+    		this.head = newNode;
+    	} else {
+    		Node prevNode = this.head;
+    		Node subsNode = null;
+    		int cnt = 1; 
+    		
+    		while(cnt < index) {
+    			
+    			if (cnt == index - 1) {
+    				newNode.setNext(prevNode.getNext());
+    				prevNode.setNext(newNode);
+    				break;
+    			} else {
+    				prevNode = prevNode.getNext();
+    				cnt++;
+    			}
+    		}// for
+    	}// else
+    	this.size++;
+    	
+//    	StackTraceElement[] stacks = new Throwable().getStackTrace();
+//    	StackTraceElement currStack = stacks[0];
+//    	System.out.println("==" + currStack.getMethodName());
+    	
+    	printList("insert");
     	
     }// insert()
 
     public Node removeFirst(){
     	
+    	Node result = this.head;
+    	
+    	if (this.head == null) {
+    		System.out.println("There is no Node to remove");
+    		return null;
+    	} else {
+    		Node nextHead = this.head.getNext();
+    		this.head = nextHead;
+    	}
     	this.size--;
-    }
+    	printList("removeFirst");
+    	return result;
+    }// removeFirst()
 
     public Node removeLast(){
-
+    	Node result = null;
+    	
+    	if (this.head == null) {
+    		System.out.println("There is no Node to remove");
+    		return null;
+    	} else {
+    		Node nextLast = this.head;
+    		Node prevNode = null;
+    		while (nextLast.getNext() != null) {
+    			prevNode = nextLast;
+    			nextLast = nextLast;
+    			result = nextLast;
+    		}
+    		prevNode.setNext(null);
+    	}
     	this.size--;
-    }
+    	printList("removeLast");
+    	return result;
+    }// removeLast()
 
     public Node remove(int index){
 
@@ -72,19 +132,24 @@ public class LinkeList {
 
     public Node get(int index){
 
-    }
+    	Node result = null;
+    	int cnt = 0;
+    	while (cnt < index) {
+    		if (cnt == index) {
+    			
+    		}
+    		cnt++;
+    	}
+    	return result;
+    }// get()
 
-    public boolean isEmpty(){
-
-    }
-
-    public void printList(){
+    public void printList(String method){
 
     	if (this.head == null) {
     		System.out.println("There is no Node..");
     	} else {
     		Node node = this.head;
-    		System.out.print(this.size + " : ");
+    		System.out.print(method + " (current size : " + this.size  + ") \n\t\t");
     		while (node != null) {
     			System.out.print(node.getValue() + "  ");
     			node = node.getNext();
--- src/com/acorn/linkedlist/TestMain.java
+++ src/com/acorn/linkedlist/TestMain.java
@@ -7,9 +7,20 @@ public class TestMain {
 		LinkeList list = new LinkeList();
 		list.addLast("Monday");
 		list.addLast("Tuesday");
-		list.addLast("Wednesday");
+		list.addLast("Friday");
 		
 		list.addFirst("Sunday");
+		
+		list.insert(4, "Wednesday");
+		list.insert(5, "Thursday");
+		
+		Node removeFirst1 = list.removeFirst();
+		System.out.println("removed : " + removeFirst1.getValue());
+		
+		Node removeLast1 = list.removeLast();
+		System.out.println("removed : " + removeLast1.getValue());
+		
+		
 		
 	}// main()
 
