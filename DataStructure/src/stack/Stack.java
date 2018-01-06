9ec4dcc982415162a30b6a0d8bd15fbe5d4bd175
Nelson Kim <ssipflow@gmail.com>
2018-01-07 01:10:18 +0900

    Stack solution commit

--- DataStructure/src/stack/Stack.java
+++ DataStructure/src/stack/Stack.java
@@ -1,23 +1,89 @@
 package stack;
 
-public class Stack {
+public class  Stack {
 
     private Node head;
-    private Node top;
+    private int top;
+
+    public Stack(){
+        this.head   = new Node();
+        this.top    = -1;
+    }
 
     public void push(Node input){
-
+        if(isEmpty()) {
+            this.head.setNext(input);
+            this.top++;
+            System.out.println("PUSH:::: Push node" + input.getValue() + " || Current top " + this.top);
+        } else {
+            Node tmp = this.head;
+            for(int i = 0; i <= this.top; i++)  tmp = tmp.getNext();
+            tmp.setNext(input);
+            this.top++;
+            
+            System.out.println("PUSH:::: Push node" + input.getValue() + " || Current top " + this.top);
+        }
+        printStack();
     }
 
     public Node pop(){
+        Node result = null;
+        
+        if(isEmpty()) {
+            System.out.println("Error:::: Stack is Empty");
+        } else {
+            Node prev = this.head;
+            Node tmp = this.head.getNext();
+            while( tmp.getNext() != null) {
+                prev = prev.getNext();
+                tmp = tmp.getNext();
+            }
+            result = tmp;
+            prev.setNext(null);
+            this.top--;
 
+            System.out.println("POP:::: Pop node " + result.getValue() + " || Current top " + this.top);
+        }
+        printStack();
+        
+        return result;
     }
 
     public Node peek(){
+        Node result = null;
+    
+        if(isEmpty()) {
+            System.out.println("Error:::: Stack is Empty");
+        } else {
+            Node tmp = this.head.getNext();
+            while (tmp.getNext() != null) {
+                tmp = tmp.getNext();
+            }
+            result = tmp;
 
+            System.out.println("PEEK:::: Peek node " + result.getValue() + " || Current top " + this.top);
+        }
+        printStack();
+        
+        return result;
     }
 
-    public boolean isEmpty(){
-
+    private boolean isEmpty(){
+        if(this.top < 0)    return true;
+        else                return false;
+    }
+    
+    private void printStack(){
+        if(isEmpty()) {
+            System.out.println("PrintStack:::: Stack is Empty");
+        } else {
+            Node tmp = this.head.getNext();
+            System.out.print("PrintStack::::");
+            for(int i = 0; i <= this.top; i++) {
+                System.out.print(tmp.getValue() + " ");
+                tmp = tmp.getNext();
+            }
+            System.out.println();
+        }
     }
 }
---
+++ DataStructure/src/stack/StackSolution.java
@@ -0,0 +1,18 @@
+package stack;
+
+public class StackSolution {
+    public static void main(String[] args){
+        Stack stack = new Stack();
+
+        stack.pop();
+        stack.push(new Node(1));
+        stack.push(new Node(2));
+        stack.push(new Node(3));
+
+        stack.peek();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+    }
+}
