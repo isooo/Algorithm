30f64f91cef4427f71c7af79758c984bf5b3d3b5
isooo <midnightmarching@gmail.com>
2018-01-20 18:39:47 +0900

    Merge partially from master

---
+++ DataStructure/src/queue/Queue.java
@@ -0,0 +1,35 @@
+package queue;
+
+public class Queue {
+    private Node head;
+    private int front;
+    private int rear;
+
+    /**
+     * 데이터 입력
+     * @param input
+     */
+    public void enQueue(Node input){
+
+    }
+
+    /**
+     * 데이터 리턴 및 삭제
+     * @return
+     */
+    public Node deQueue(){
+
+    }
+
+    /**
+     * 데이터 리턴
+     * @return
+     */
+    public Node element(){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+}
