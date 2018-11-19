e618ce7e996a43f98b9049d8001982b06f4a642e
isooo <midnightmarching@gmail.com>
2018-11-19 15:22:00 +0900

    ch01-Q17 : n단의 숫자피라미드 출력하기

---
+++ Doit_java/src/ch01/Q17.java
@@ -0,0 +1,34 @@
+package ch01;
+
+import java.util.Scanner;
+
+/*
+    숫자를 늘어놓아 피라미드를 출력
+ */
+
+public class Q17 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("숫자 피라미드 출력하기");
+        System.out.print("몇 단입니까? : ");
+        int n = sc.nextInt();
+        nspira(n);
+
+    }
+
+    static void nspira (int n) {
+        for (int i = 1 ; i <= n ; i++) {
+            for (int j = n ; j > i ; j-- ) {
+                // for (int j = 1; j <= n - i + 1; j++)
+                System.out.print(' ');
+            }
+            for (int j = 1 ; j <= (i - 1)* 2 + 1 ; j++) {
+                System.out.print(i % 10);
+            }
+            System.out.println();
+        }
+    }
+
+}
