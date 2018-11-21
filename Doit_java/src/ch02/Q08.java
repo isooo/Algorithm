e76220cef37ae18897f630d080c4e67a38a4f6f7
isooo <midnightmarching@gmail.com>
2018-11-22 03:32:40 +0900

    ch02-Q09 : 남은 일 수 계산하기

--- Doit_java/src/ch02/Q08.java
+++ Doit_java/src/ch02/Q08.java
@@ -24,14 +24,31 @@ public class Q08 {
         return d;
     }
 
+    static int leftDayOfYear(int y, int m, int d) {
+        int days = mdays[isLeap(y)][m-1] - d;
+        for (int i = 12 ; i > m ; i--) {
+            days += mdays[isLeap(y)][i - 1];
+        }
+        return days;
+    }
+
+    static int leftDayOfYearAnswer(int y, int m, int d) {
+        int days = d; // 일수
+
+        for (int i = 1; i < m; i++) // 1월~(m-1)월의 일 수를 더함
+            days += mdays[isLeap(y)][i - 1];
+        return 365 + isLeap(y) - days;
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int retry;
 
-        System.out.println("그 해 결과 일수를 구합니다.");
-
         do{
+            System.out.print("1. 경과 일 수 구하기 / 2. 남은 일 수 구하기 : ");
+            int select = sc.nextInt();
+
             System.out.print("년 : ");
             int year = sc.nextInt();
             System.out.print("월 : ");
@@ -39,7 +56,11 @@ public class Q08 {
             System.out.print("일 : ");
             int day = sc.nextInt();
 
-            System.out.printf("그 해 %d일째입니다.\n", dayOfYear(year, month, day));
+            if (select == 1) {
+                System.out.printf("그 해 %d일째입니다.\n", dayOfYear(year, month, day));
+            } else {
+                System.out.println(year + "년은 " + leftDayOfYear(year, month, day) + "일이 남았습니다.");
+            }
 
             System.out.print("한번 더 할까요? (1. 예 / 0. 아니오) : ");
             retry = sc.nextInt();
