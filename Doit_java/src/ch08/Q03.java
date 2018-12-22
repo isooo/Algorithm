860b3b0a25516405a77c4663f6f66a9d7a9f0947
isooo <midnightmarching@gmail.com>
2018-12-22 17:24:10 +0900

    ch08-Q03 : KMP법으로 문자열 찾기 + 검색과정 출력하기

--- Doit_java/src/ch08/Q03.java
+++ Doit_java/src/ch08/Q03.java
@@ -3,43 +3,88 @@ package ch08;
 import java.util.Scanner;
 
 public class Q03 {
+    static int kmpMatch(String txt, String pat) {
+        int pt = 1; // txt를 따라가는 커서
+        int pp = 0; // pat를 따라가는 커서
+        int count = 0; // 비교횟수
+        int[] skip = new int[pat.length() + 1]; // skip 테이블
+        int k = -1;
 
-    public static int kmpMatch(String txt, String pat) {
-        int pt = 1;                             // txt 커버
-        int pp = 0;                             // pat 커서
-        int[] skip = new int[pat.length() + 1];     // 건너뛰기 표
-
-        // 건너뛰기용 표
+        // skip 테이블 만들기
+        System.out.println("skip 테이블 만들기");
         skip[pt] = 0;
-        while(pt != pat.length()) {
-            if(pat.charAt(pt) == pat.charAt(pp)) {
-                skip[++pt] = ++pp;
-            } else if(pp == 0) {
-                skip[++pt] = pp;
-            } else {
-                pp = skip[pp];
+        while (pt != pat.length()) {
+            if (k == pt - pp)
+                System.out.print("    ");
+            else {
+                System.out.printf("%2d  ", pt - pp);
+                k = pt - pp;
             }
-        }
+            for (int i = 0; i < txt.length(); i++)
+                System.out.print(txt.charAt(i) + " ");
+            System.out.println();
 
-        pt = pp = 0;
+            for (int i = 0; i < pt * 2 + 4; i++)
+                System.out.print(" ");
+            System.out.print(txt.charAt(pt) == pat.charAt(pp) ? '+' : '|');
+            System.out.println();
+
+            for (int i = 0; i < (pt - pp) * 2 + 4; i++)
+                System.out.print(" ");
+
+            for (int i = 0; i < pat.length(); i++)
+                System.out.print(pat.charAt(i) + " ");
+            System.out.println();
+            System.out.println();
+            count++;
+            if (pat.charAt(pt) == pat.charAt(pp))
+                skip[++pt] = ++pp;
+            else if (pp == 0)
+                skip[++pt] = pp;
+            else
+                pp = skip[pp];
+        }
 
         // 검색
-        while(pt != txt.length() && pp != pat.length()) {
-            if(txt.charAt(pt) == pat.charAt(pp)) {
+        System.out.println("검색");
+        pt = pp = 0;
+        while (pt != txt.length() && pp != pat.length()) {
+            if (k == pt - pp)
+                System.out.print("    ");
+            else {
+                System.out.printf("%2d  ", pt - pp);
+                k = pt - pp;
+            }
+            for (int i = 0; i < txt.length(); i++)
+                System.out.print(txt.charAt(i) + " ");
+            System.out.println();
+
+            for (int i = 0; i < pt * 2 + 4; i++)
+                System.out.print(" ");
+            System.out.print(txt.charAt(pt) == pat.charAt(pp) ? '+' : '|');
+            System.out.println();
+
+            for (int i = 0; i < (pt - pp) * 2 + 4; i++)
+                System.out.print(" ");
+
+            for (int i = 0; i < pat.length(); i++)
+                System.out.print(pat.charAt(i) + " ");
+            System.out.println();
+            System.out.println();
+            count++;
+            if (txt.charAt(pt) == pat.charAt(pp)) {
                 pt++;
                 pp++;
-            } else if(pp == 0) {
+            } else if (pp == 0)
                 pt++;
-            } else {
+            else
                 pp = skip[pp];
-            }
         }
 
-        if(pp == pat.length()) {
-            return pt - pp;         // pt - pp를 반환
-        }
-
-        return -1;                  // 검색 실패
+        System.out.printf("비교는 %d회입니다.\n", count);
+        if (pp == pat.length()) // 패턴의 모든 문자를 조사
+            return pt - pp;
+        return -1; // 검색실패
     }
 
 
