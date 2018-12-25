72f0d42c741f23dc087b49a77f4165c8d3ba3e5c
isooo <midnightmarching@gmail.com>
2018-12-26 00:11:11 +0900

    ch09-Q01 : LinkedList 간단한 메소드 구현

--- Doit_java/src/ch09/Q01.java
+++ Doit_java/src/ch09/Q01.java
@@ -45,4 +45,108 @@ class LinkedList<E> {
     }
 
     // 머리에 노드 삽입
+    public void addFirst(E obj) {
+        Node<E> ptr = head;
+        head = crnt = new Node<E>(obj, ptr);
+    }
+
+    // 꼬리에 노드 삽입
+    public void addLast(E obj) {
+        if(head == null) {      // 리스트가 비어있으면
+            addFirst(obj);      // 머리에 삽입
+        } else {
+            Node<E> ptr = head;
+            while(ptr.next != null) {
+                ptr = ptr.next;
+            }
+            ptr.next = crnt = new Node<E>(obj, null);
+        }
+    }
+
+    // 머리 노드를 삭제
+    public void removeFirst() {
+        if(head != null) {
+            head = crnt = head.next;
+        }
+    }
+
+    // 꼬리 노드를 삭제
+    public void removeLast() {
+        if(head != null) {
+            if(head.next == null) {     // 노드가 하나만 있으면
+                removeFirst();          // 머리 노드를 삭제
+            } else {
+                Node<E> ptr = head;     // 스캔 중인 노드
+                Node<E> pre = head;     // 스캔 중인 노드의 앞쪽 노드
+
+                while(ptr.next != null) {
+                    pre = ptr;
+                    ptr = ptr.next;
+                }
+                pre.next = null;        // pre는 삭제 후의 꼬리 노드
+                crnt = pre;
+            }
+        }
+    }
+
+    // 노드 삭제
+    public void remove(Node target) {
+        if(head != null) {
+            if(target == head) {
+                removeFirst();
+            } else {
+                Node<E> ptr = head;
+
+                while(ptr.next != target) {
+                    ptr = ptr.next;
+                    if(ptr == null) {
+                        return;         // target이 리스트에 없는 경우
+                    }
+                }
+                ptr.next = target.next;
+                crnt = ptr;
+            }
+        }
+    }
+
+    // 현재 선택된 노드 삭제
+    public void removeCurrentNode() {
+        remove(crnt);
+    }
+
+    // 모든 노드를 삭제
+    public void clear() {
+        while(head != null) {
+            removeFirst();      // 노드에 아무것도 없을 때까지 머리 노드를 삭제
+        }
+        crnt = null;
+    }
+
+    // 선택 노드 crnt를 하나 뒤로 이동
+    public boolean next() {
+        if(crnt == null || crnt.next == null) {
+            return false;
+        }
+        crnt = crnt.next;
+        return true;
+    }
+
+    // 선택 노드 crnt를 출력
+    public void printCurrentNode() {
+        if(crnt == null) {
+            System.out.println("선택한 노드가 없습니다.");
+        } else {
+            System.out.println("선택된 노드 : " + crnt.data);
+        }
+    }
+
+    // 모든 노드를 출력
+    public void dump() {
+        Node<E> ptr = head;
+
+        while(ptr != null) {
+            System.out.println("\t" + ptr.data);
+            ptr = ptr.next;
+        }
+    }
 }
