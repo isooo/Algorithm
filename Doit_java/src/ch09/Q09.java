ba0c05c686915e7c0bc1403facd7e3a1076b78d9
isooo <midnightmarching@gmail.com>
2019-01-01 15:17:53 +0900

    ch09-Q09 : 원형 이중 연결리스트 만들기 시작

---
+++ Doit_java/src/ch09/Q09.java
@@ -0,0 +1,53 @@
+package ch09;
+
+import java.util.Comparator;
+
+public class Q09 {
+}
+
+class DbLinkedList<E> {
+
+    class Node<E> {
+        private E data;
+        private Node<E> prev;
+        private Node<E> next;
+
+        Node() {
+            data = null;
+            prev = next = this;
+        }
+
+        Node(E obj, Node<E> prev, Node<E> next) {
+            data = obj;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private Node<E> head;
+    private Node<E> crnt;
+
+    public DbLinkedList() {
+        head = crnt = new Node<E>();
+    }
+
+    public boolean isEmpty() {
+        return head.next == head;
+    }
+
+    public E search(E obj, Comparator<? super E> c) {
+        Node<E> ptr = head.next;
+
+        while(ptr != head) {
+            if(c.compare(obj, ptr.data) == 0) {
+                crnt = ptr;
+                return ptr.data;
+            }
+            ptr = ptr.next;
+        }
+        return null;
+    }
+
+
+
+}
