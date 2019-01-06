599700cba4f72b8729abfcde64408f6e1dba9b5d
isooo <midnightmarching@gmail.com>
2019-01-06 17:27:59 +0900

    ch10-Q01 : 이진검색트리 만들기 : 삭제, 출력

--- Doit_java/src/ch10/Q01.java
+++ Doit_java/src/ch10/Q01.java
@@ -3,6 +3,23 @@ package ch10;
 import java.util.Comparator;
 
 public class Q01 {
+    public static void main(String[] args) {
+
+        BinTree<Integer, String> bin = new BinTree<>();
+
+        bin.add(1, "일");
+        bin.add(2, "이");
+        bin.add(8, "팔");
+        bin.add(6, "육");
+        bin.add(3, "삼");
+        bin.add(5, "오");
+        bin.add(7, "칠");
+        bin.add(4, "사");
+
+        bin.print();
+        System.out.println("=================================");
+        bin.printReverse();
+    }
 }
 
 class BinTree<K, V> {
@@ -103,6 +120,98 @@ class BinTree<K, V> {
             root = new Node<K, V>(key, data, null, null);
         } else {
             addNode(root, key, data);
+        }
+    }
+
+    // 키 값이 key인 노드를 삭제
+    public boolean remove(K key) {
+        Node<K, V> p = root;                    // 스캔 중인 노드
+        Node<K, V> parent = null;               // 스캔 중인 노드의 부모 노드
+        boolean isLeftChild = true;             // p는 부모의 왼쪽 자식 노드인가?
+
+        while(true) {
+            if(p == null) {                     // 더 이상 진행하지 않으면
+                return false;                   // 그 키 값은 없습니다
+            }
+
+            int compResult = comp(key, p.getKey());     // key와 노드 p의 키 값을 비교
+            if(compResult == 0) {
+                break;                                  // 검색 성공
+            } else {
+                parent = p;
+                if(compResult < 0) {                    // key가 작고 스캔 중인 노드가 크면
+                    isLeftChild = true;                 // 왼쪽 자식으로 내려감
+                    p = p.left;
+                } else {
+                    isLeftChild = false;
+                    p = p.right;
+                }
+            }
+        }
+
+        if(p.left == null) {                    // p에는 왼쪽 자식이 없음
+            if(p == root) {
+                root = p.right;
+            } else if(isLeftChild) {
+                parent.left = p.right;          // p가 부모의 왼쪽 자식일 경우, 부모의 왼쪽 포인터가 p의 오른쪽 자식을 가리킴
+            } else {
+                parent.right = p.right;         // 부모의 오른쪽 포인터가, p의 오른쪽 자식을 가리킴 (== p가 삭제됨)
+            }
+        } else if(p.right == null) {
+            if(p == root) {
+                root = p.left;
+            } else if(isLeftChild) {
+                parent.left = p.left;
+            } else {
+                parent.right = p.left;
+            }
+        } else {
+            parent = p;
+            Node<K, V> left = p.left;           // 왼쪽 서브트리 가운데 가장 큰 노드
+            isLeftChild = true;
+            while(left.right != null) {         // p의 왼쪽 서브트리 가운데 가장 큰 노드를 찾음
+                parent = left;
+                left = left.right;
+                isLeftChild = false;
+            }
+
+            p.key = left.key;                   // left의 key값을 p로 옮김
+            p.data = left.data;                 // left의 데이터를 p로 옮김
+
+            if(isLeftChild) {
+                parent.left = left.left;
+            } else {
+                parent.right = left.left;
+            }
+        }
+        return true;
+    }
+
+    // target을 루트로 하는 서브 트리의 노드를 키 값의 오름차순으로 출력
+    private void printSubTree(Node target) {
+        if(target != null) {
+            printSubTree(target.left);          // 왼쪽 서브 트리를 키 값의 오름차순으로 출력
+            System.out.println(target.key + " : " + target.data);
+            printSubTree(target.right);         // 오른쪽 서브 트리를 키 값의 오름차순으로 출력
+        }
+    }
+
+    // 모든 노드를 키 값의 오름차순으로 출력
+    public void print() {
+        printSubTree(root);
+    }
+
+    // 모든 노드를 키 값의 내림차순으로 출력하기
+    public void printReverse() {
+        printSubTreeR(root);
+    }
+
+    // target을 루트로 하는 서브 트리의 노드를, 키 값의 내림차순으로 출력
+    private void printSubTreeR(Node target) {
+        if(target != null) {
+            printSubTreeR(target.right);
+            System.out.println(target.key + " : " + target.data);
+            printSubTreeR(target.left);
         }
     }
 }
