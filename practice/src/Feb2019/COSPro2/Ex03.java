90fceb2ddc89b361ad60fb9e302aeda11eb1eddb
isooo <midnightmarching@gmail.com>
2019-02-21 12:04:47 +0900

    체스 나이트의 이동 문제 해설 추가

--- practice/src/Feb2019/COSPro2/Ex03.java
+++ practice/src/Feb2019/COSPro2/Ex03.java
@@ -1,10 +1,25 @@
-/*
 package Feb2019.COSPro2;
-
+/*
+    체스 나이트(knight)의 이동
+ */
 public class Ex03 {
     public int solution(String pos) {
+
+        int[] xPattern = {-2, -2, -1, -1, 1, 1, 2, 2};
+        int[] yPattern = {-1, 1, -2, 2, -2, 2, -1, 1};
+
+        int x = pos.charAt(0) - 'A';
+        int y = pos.charAt(1) - '0' - 1;
+
         int answer = 0;
 
+        for(int i = 0 ; i < 8 ; i++) {
+            int xTmp = x + xPattern[i];
+            int yTmp = y + yPattern[i];
+            if((0 <= xTmp && xTmp < 8) && (0 <= yTmp && yTmp < 8)) {
+                answer++;
+            }
+        }
         return answer;
     }
 
@@ -18,4 +33,3 @@ public class Ex03 {
         System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
     }
 }
-*/
