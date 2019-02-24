53443f7ea4876a36781af1e2df218061271e4d7b
isooo <midnightmarching@gmail.com>
2019-02-24 23:57:11 +0900

    acmicpc 1920 : 수 찾기 1 : 입력값 다 받고나서, for문으로 하나하나 비교 2 : 대상이 되는 입력값만 받고나서, 찾아야하는 배열은 입력받을 때마다 Set 컬렉션 이용하여 바로 찾

---
+++ practice/src/Feb2019/acmicpc/Ex1920.java
@@ -0,0 +1,39 @@
+package Feb2019.acmicpc;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex1920 {
+    public static void solution(int[] arr, int[] arr2) {
+        int idx1 = 0;
+        int idx2 = 0;
+
+        for(int i = 0, j = 0  ; i < arr2.length ; i++) {
+            for(j = 0 ; j < arr.length ; j++) {
+                if(arr2[i] == arr[j]) {
+                    System.out.println(1);
+                    break;
+                }
+            }
+            if (j == arr.length) {
+                System.out.println(0);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int n2 = sc.nextInt();
+        int[] arr2 = new int[n2];
+        for(int i = 0 ; i < n2 ; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        solution(arr, arr2);
+    }
+}
---
+++ practice/src/Feb2019/acmicpc/Ex1920_2.java
@@ -0,0 +1,23 @@
+package Feb2019.acmicpc;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+/*
+    N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때,
+    이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오
+ */
+public class Ex1920_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0 ; i < n ; i++) {
+            set.add(sc.nextInt());
+        }
+        n = sc.nextInt();
+        for(int i = 0 ; i < n ; i++) {
+            System.out.println(set.contains(sc.nextInt())? 1 : 0);
+        }
+    }
+}
