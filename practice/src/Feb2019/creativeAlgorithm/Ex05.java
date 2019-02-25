2848824bea7b7d463b1b8f61ac850305d3826b9a
isooo <midnightmarching@gmail.com>
2019-02-25 14:11:54 +0900

    문제해결을 위한 창의적 알고리즘(중급) p62 upper bound n개로 이루어진 정수 집합에서 원하는 수 k보다 큰 수가 처음으로 등장하는 위치를 찾으시오.

---
+++ practice/src/Feb2019/creativeAlgorithm/Ex05.java
@@ -0,0 +1,39 @@
+package Feb2019.creativeAlgorithm;
+
+import java.util.Scanner;
+
+/*
+    upper bound
+    n개로 이루어진 정수 집합에서
+    원하는 수 k보다 큰 수가 처음으로 등장하는 위치를 찾으시오.
+ */
+public class Ex05 {
+    public static int solution(int[] arr, int target) {
+        int mid = 0;
+        int pl = 0;
+        int pr = arr.length - 1;
+        while(pr - pl > 0) {
+            mid = (pr + pl) / 2;
+            if(arr[mid] <= target) {
+                pl = mid + 1;
+            } else {
+                pr = mid;
+            }
+        }
+        if(arr[pr] <= target) {
+            pr++;
+        }
+        return pr + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        int[] arr = new int[cnt];
+        for(int i = 0 ; i < cnt ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        System.out.println("result : " + solution(arr, target));
+    }
+}
