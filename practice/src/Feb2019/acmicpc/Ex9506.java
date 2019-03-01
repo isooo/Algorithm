bd527ea3da9e5d8d0442419dd9ef1add6dfbd484
isooo <midnightmarching@gmail.com>
2019-03-01 23:38:25 +0900

    acmicpc 9506 : 약수들의 합 어떤 숫자 n이 자신을 제외한 모든 약수들의 합과 같으면, 그 수를 완전수라고 한다. 예를 들어 6은 6 = 1 + 2 + 3 으로 완전수이다. n이 완전수인지 아닌지 판단해주는 프로그램을 작성하라.
    1. 처음에는 List에다가 해당되는 요소(i)를 담았고,
    n보다 작을때까지 반복을 시켰다.
    
    2. 해당되는 요소는 StringBuilder에다 append하였고
    n/2 로 반복 횟수를 줄였다.
    
    속도면에서는 큰 차이가 없었다..
    메모리는 조금 줄어들었다.

---
+++ practice/src/Feb2019/acmicpc/Ex9506.java
@@ -0,0 +1,65 @@
+package Feb2019.acmicpc;
+
+import java.util.*;
+
+public class Ex9506 {
+    private static void solution(List<Integer> list) {
+        Iterator iter = list.iterator();
+        while(iter.hasNext()) {
+            List<Integer> ele = new ArrayList<>();
+            int n = (Integer)(iter.next());
+            int result = 0;
+            for(int i = 1 ; i < n ; i++) {
+                if(n % i == 0) {
+                    result += i;
+                    ele.add(i);
+                }
+            }
+            System.out.print(n);
+            if(result == n) {
+                System.out.print(" = ");
+                for(int i = 0 ; i < ele.size() - 1 ; i++) {
+                    System.out.print(ele.get(i) + " + ");
+                }
+                System.out.println(ele.get(ele.size() - 1));
+            } else {
+                System.out.println(" is NOT perfect.");
+            }
+        }
+    }
+
+    private static void solution2(List<Integer> list) {
+        Iterator<Integer> iter = list.iterator();
+        while(iter.hasNext()) {
+            StringBuilder sb = new StringBuilder();
+            int n = iter.next();
+            sb.append(n).append(" = 1");
+            int result = 1;
+            for(int i = 2 ; i <= n/2 ; i++) {
+                if(n % i == 0) {
+                    result += i;
+                    sb.append(" + ").append(i);
+                }
+            }
+            if(result == n) {
+                System.out.println(sb.toString());
+            } else {
+                System.out.println(n + " is NOT perfect.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        while(true) {
+            int n = sc.nextInt();
+            if(n == -1) {
+                break;
+            } else {
+                list.add(n);
+            }
+        }
+        solution2(list);
+    }
+}
