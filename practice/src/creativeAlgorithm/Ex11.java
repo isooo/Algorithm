639ab164553c93c202ef42a70c72136618ad6466
isooo <midnightmarching@gmail.com>
2019-03-02 03:07:34 +0900

    문제해결을 위한 창의적 알고리즘(중급) p115 : 삼각화단 만들기 수정
    처음엔 for문을 3개 써서 구현하였으나, 시간 초과로....
    for문2개 쓰는 코드로 변경!
    1. a의 범위를 /2 으로 줄이고
    2. b의 범위를 (n-a)/2 로 줄이고 --, 그리고 a보다 크거나 같을 때까지!
    3. 만족하지 않으면 break
    
    c가 가장 긴 변이라 가정하고
    a부터 증가시킬 때,
    b의 경우엔 b가 가질 수 있는 가장 큰 값부터 내려오는 순이다.
    (n - a)/2 를 하였기때문에, c보다는 크지않은 후보들 가운데 가장 확률이 높은 숫자부터 체크하는 것이다.
    그렇게 내려오다가 a보다 크거나 같은 숫자까지만 체크하고 (그래야 중복이 일어나지 않음)
    조건식에 어긋날 경우 break를 걸어
    a를 중가시키고 새로운 루프를 돌게된다.

--- practice/src/creativeAlgorithm/Ex11.java
+++ practice/src/creativeAlgorithm/Ex11.java
@@ -7,22 +7,41 @@ import java.util.Scanner;
 public class Ex11 {
     private static int solution(int n) {
         int result = 0;
-        for(int a = 0 ; a < n ; a++) {
-            for(int b = a ; b < n ; b++) {
-                for(int c = b ; c < n ; c++) {
-                    if(a + b > c && a + b + c == n) {
+        for(int a = 1 ; a <= n / 2 ; a++) {
+            for(int b = (n - a) / 2 ; b >= a ; b--) {
+//                for(int c = b ; c < n ; c++) {
+                int c = n - a - b;
+                    if(a + b > c) {
                         System.out.println(a + ", " + b + ", " + c);
                         result++;
+                    } else {
+                        break;
                     }
-                }
+//                }
             }
         }
         return result;
     }
+//    private static int cnt;
+//    private static int[][][] chk = new int[100][100][100];
+//    private static void solution2(int n, int a, int b, int c) {
+//        if(a + b + c == n) {
+//            if(a <= b && b <= c && a+ b > c && chk[a][b][c] == 0) {
+//                cnt++;
+//                chk[a][b][c] = 1;
+//            }
+//            return;
+//        }
+//        solution2(n, a + 1, b, c);
+//        solution2(n, a, b + 1, c);
+//        solution2(n, a, b, c + 1);
+//    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(solution(n));
+//        solution2(n,1, 1, 1);
+//        System.out.println(cnt);
     }
 }
