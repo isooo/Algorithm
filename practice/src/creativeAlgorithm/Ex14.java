54cf87cb31aebf3a248d99717829070e57a84382
isooo <midnightmarching@gmail.com>
2019-03-05 13:18:42 +0900

    문제해결을 위한 창의적 알고리즘(중급) p128 : 오목 오목판이 입력되었을 때, 이긴 돌과 (검은돌 1, 흰돌2), 연속된 바둑알 중 가장 왼쪽(이면서 가장 위쪽)인 좌표를 출력하기. 6줄은 오목이 아님. 오직 5줄 --- 연속된 돌을 찾았을 때, 이게 그 전 좌표부터 이어진건지 검사하고 그게 아니라면 5줄인지 검사하

---
+++ practice/src/creativeAlgorithm/Ex14.java
@@ -0,0 +1,99 @@
+package creativeAlgorithm;
+
+import java.util.Scanner;
+
+/*
+    오목
+        1. 0, 0)부터 (18, 18)까지 행 우선으로 탐색을 시작한다.
+        2. 현재 탐색 중인 돌을 기준으로 오목이 완성되었는지 검사한다.
+        3. 완성되지 않았으면 탐색을 진행하기 위해 2번으로 간다.
+        4. 완성된 돌의 위치와 색깔을 출력한다.
+ */
+public class Ex14 {
+    private static void solution(int[][] arr) {
+        for(int i = 0 ; i < 19 ; i++) {
+            for(int j = 0 ; j < 19 ; j++) {
+                if(arr[i][j] != 0) {
+                    if(arr[i][j - 1] != arr[i][j] && search1(arr, arr[i][j], i, j)) {
+                        System.out.printf("%d\n%d %d", arr[i][j], (i+1), (j+1));
+                    }       // ->
+                    if(arr[i - 1][j - 1] != arr[i][j] && search2(arr, arr[i][j], i, j)) {
+                        System.out.printf("%d\n%d %d", arr[i][j], (i+1), (j+1));
+                    }       // \
+                    if(arr[i - 1][j] != arr[i][j] && search3(arr, arr[i][j], i, j)) {
+                        System.out.printf("%d\n%d %d", arr[i][j], (i+1), (j+1));
+                    }       // |
+                    if(arr[i + 1][j - 1] != arr[i][j] && search4(arr, arr[i][j], i, j)) {
+                        System.out.printf("%d\n%d %d", arr[i][j], (i+1), (j+1));
+                    }       // /
+                }
+            }
+        }
+    }
+
+    private static boolean search1(int[][]arr, int color, int i, int j) {
+        int cnt = 1;
+        for(; color == arr[i][j+1]; j++) {
+            cnt++;
+        }
+        return cnt == 5 ? true : false;
+    }
+    private static boolean search2(int[][]arr, int color, int i, int j) {
+        int cnt = 1;
+        for(; color == arr[i+1][j+1]; i++, j++) {
+            cnt++;
+        }
+        return cnt == 5 ? true : false;
+    }
+    private static boolean search3(int[][]arr, int color, int i, int j) {
+        int cnt = 1;
+        for(; color == arr[i+1][j]; i++) {
+            cnt++;
+        }
+        return cnt == 5 ? true : false;
+    }
+    private static boolean search4(int[][]arr, int color, int i, int j) {
+        int cnt = 1;
+        for(; color == arr[i-1][j+1]; i--, j++) {
+            cnt++;
+        }
+        return cnt == 5 ? true : false;
+    }
+
+    public static void main(String[] args) {
+/*        int[][] arr = new int[19][19];
+
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0 ; i < 19 ; i++) {
+            String[] tmp = sc.nextLine().split(" ");
+            for(int j = 0 ; j < 19 ; j++) {
+                arr[i][j] = Integer.parseInt(tmp[j]);
+            }
+        }
+*/
+
+        int[][] arr = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 2, 0, 0, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        solution(arr);
+    }
+}
