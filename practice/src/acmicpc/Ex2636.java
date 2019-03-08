19a3f5b26decb8ac2726814261c0bdeb4f53b3ef
isooo <midnightmarching@gmail.com>
2019-03-08 20:30:56 +0900

    acmicpc 2636 : 치즈 입력으로 사각형 모양의 판의 크기와 한 조각의 치즈가 판 위에 주어졌을 때, 공기 중에서 치즈가 모두 녹아 없어지는 데 걸리는 시간과 모두 녹기 한 시간 전에 남아있는 치즈조각이 놓여 있는 칸의 개수 를 구하는 프로그램을 작성하시오. -- 깊이우선탐색 이

---
+++ practice/src/acmicpc/Ex2636.java
@@ -0,0 +1,97 @@
+package acmicpc;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex2636 {
+    private static int n;
+    private static int m;
+    private static int[][] arr;
+    private static int lastCheese;
+
+    private static boolean inside(int x, int y) {
+        return ((0 <= x && x < n)&&(0 <= y && y < m));
+    }
+
+    private static boolean done() {
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(arr[i][j] == -1 || arr[i][j] >= 2) {
+                    arr[i][j] = 0;
+                } else if(arr[i][j] == 1) {
+                    cnt++;
+                }
+            }
+        }
+        if(cnt != 0) {
+          lastCheese = cnt;
+        }
+        return cnt==0;
+    }
+
+    private static void solve(int x, int y) {
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        arr[x][y] = -1;
+        for(int i=0; i<4; i++) {
+            if(inside(x+dx[i], y+dy[i])) {
+                if(arr[x+dx[i]][y+dy[i]] == 0) {
+                    solve(x+dx[i], y+dy[i]);
+                } else if(arr[x+dx[i]][y+dy[i]] > 0) {
+//                    System.out.println("\tarr[" + (x+dx[i]) + "][" + (y+dy[i]) + "] = " + arr[x+dx[i]][y+dy[i]]);
+                    arr[x+dx[i]][y+dy[i]]++;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        sc.nextLine();
+        arr = new int[n][m];
+        for(int i=0; i<n; i++) {
+            String[] str = sc.nextLine().split(" ");
+            for(int j=0; j<m; j++) {
+                arr[i][j] = Integer.parseInt(str[j]);
+            }
+        }
+
+//        n = 13;
+//        m = 12;
+//        arr = new int[][]{
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+//                {0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0},
+//                {0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+//                {0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0},
+//                {0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0},
+//                {0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0},
+//                {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+//                {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+//                {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+//                {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+//        };
+//
+
+        int result = 0;
+
+        for(; !done(); result++) {
+//            for(int i=0; i<n; i++) {
+//                System.out.println(Arrays.toString(arr[i]));
+//            }
+//            System.out.println("---------------------");
+            solve(0, 0);
+//            for(int i=0; i<n; i++) {
+//                System.out.println("\t\t" + Arrays.toString(arr[i]));
+//            }
+//            System.out.println("------------------------------------------");
+        }
+        System.out.println(result);
+        System.out.println(lastCheese);
+    }
+}
